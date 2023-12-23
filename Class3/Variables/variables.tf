5740260376274044a350b129e2f1960cefd2765a
nurkyzsultanbek <norasultanbek@gmail.com>
2023-12-23 22:40:16 +0000

    added class3 files

---
+++ Class3/Variables/variables.tf
@@ -0,0 +1,31 @@
+variable region{
+    type = string
+    description = "Provide region"
+    default = ""
+}
+
+variable vpc_cidr{
+    type = string
+    description = "Provide cidr block"
+    default = "10.0.0.0/16"
+}
+variable subnet1_cidr {
+    type = string
+    description = "Provide cidr block"
+    default = "10.0.1.0/24"
+}
+variable subnet2_cidr {
+    type = string
+    description = "Provide cidr block"
+    default = "10.0.2.0/24"
+}
+variable subnet3_cidr {
+    type = string
+    description = "Provide cidr block"
+    default = "10.0.3.0/24"
+}
+variable instance_type {
+    type = string
+    description = "Provide instance type"
+    default = "t2.micro"
+}
